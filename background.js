0cf79bb74dc2d6054e80e2f6fb1810cc39cdece9
Grisha Egorov <egorovg13@gmail.com>
2020-06-12 15:06:50 +0300

    switched to pageaction to work only on youtube

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
-// background.js
-
-// Called when the user clicks on the browser action.
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//     // Send a message to the active tab
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       var activeTab = tabs[0];
-//       chrome.tabs.sendMessage(activeTab.id, {"message": "test video title"});
-//     });
-//   });
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+        {
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { urlContains: 'youtube.com' },
+            })
+          ],
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }
+      ]);
+    });
